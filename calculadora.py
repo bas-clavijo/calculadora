c198e141e1be4cd6bde5079b312b1847126959ef
bas-clavijo <bas.clavijo@duocuc.cl>
2023-04-19 19:18:01 -0400

    feat(calculadora):Agregando botones

--- calculadora.py
+++ calculadora.py
@@ -14,3 +14,22 @@ root.geometry("+500+80")
 mainframe = ttk.Frame(root)
 mainframe.grid(column=0, row=0)
 
+entrada1 = StringVar()
+labelEntrada1 = ttk.Label(mainframe, textvariable=entrada1)
+labelEntrada1.grid(column=0, row=0)
+
+entrada2 = StringVar()
+labelEntrada2 = ttk.Label(mainframe, textvariable=entrada2)
+
+
+#Botones
+button0 = ttk.Button(mainframe,text="0")
+button1 = ttk.Button(mainframe,text="1")
+button2 = ttk.Button(mainframe,text="2")
+button3 = ttk.Button(mainframe,text="3")
+button4 = ttk.Button(mainframe,text="4")
+button5 = ttk.Button(mainframe,text="5")
+button6 = ttk.Button(mainframe,text="6")
+button7 = ttk.Button(mainframe,text="7")
+button8 = ttk.Button(mainframe,text="8")
+button9 = ttk.Button(mainframe,text="9")
