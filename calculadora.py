deb1b93aef95c2bdaba1daa90613ac068681a460
bas-clavijo <bas.clavijo@duocuc.cl>
2023-04-20 00:24:16 -0400

    feat(calculadora):Se agregan los estilos

--- calculadora.py
+++ calculadora.py
@@ -20,6 +20,7 @@ labelEntrada1.grid(column=0, row=0)
 
 entrada2 = StringVar()
 labelEntrada2 = ttk.Label(mainframe, textvariable=entrada2)
+labelEntrada2.grid(column=0, row=1)
 
 
 #Botones
@@ -49,4 +50,33 @@ buttonIgual = ttk.Button(mainframe, text="=")
 buttonRaizCuadrada = ttk.Button(mainframe, text="√")
 
 #Se muestran los botones en pantalla
+buttonParentesisIzq.grid(column=0, row=2)
+buttonParentesisDer.grid(column=1, row=2)
+buttonBorrarTodo.grid(column=2, row=2)
+buttonBorrar.grid(column=3, row=2)
 
+button7.grid(column=0, row=3)
+button8.grid(column=1, row=3)
+button9.grid(column=2, row=3)
+buttonDivision.grid(column=3, row=3)
+
+button4.grid(column=0, row=4)
+button5.grid(column=1, row=4)
+button6.grid(column=2, row=4)
+buttonMultiplicacion.grid(column=3, row=4)
+
+button1.grid(column=0, row=5)
+button2.grid(column=1, row=5)
+button3.grid(column=2, row=5)
+buttonSuma.grid(column=3, row=5)
+
+button0.grid(column=0, row=6, columnspan=2, sticky=(W,E))
+buttonPunto.grid(column=2, row=6)
+buttonResta.grid(column=3, row=6)
+
+buttonIgual.grid(column=0, row=7, columnspan=3, sticky=(W,E))
+buttonRaizCuadrada.grid(column=3, row=7)
+root.mainloop()
+
+
+#Se agregan los estilos 
